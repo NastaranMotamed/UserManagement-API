68d04c883b7f3c0566c41ceb4f57778a1c06b274
NastaranMotamed <NastaranMotamed@gmail.com>
2024-11-11 22:03:11 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-// Middleware برای تجزیه داده‌های JSON
+
 app.use(express.json());
 
-// وارد کردن روت‌های کاربران
+
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-// اتصال به MongoDB و راه‌اندازی سرور
+
 mongoose.connect('mongodb://localhost:27017/user-management')
   .then(() => {
     console.log('Connected to MongoDB');
 
-    // راه‌اندازی سرور پس از اتصال به MongoDB
+
     app.listen(3000, () => {
       console.log('Server is running on port 3000');
     });
